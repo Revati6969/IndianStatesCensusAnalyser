5bf67f5866d50b2a16619fa31b09299dd891666a
Revati Tekale <revitekale1910@gmail.com>
2020-03-30 13:14:14 +0530

    ADDED:DAO for US Census data

---
+++ src/main/java/com/bridgelabz/dao/CensusDAO.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.dao;
+
+import com.bridgelabz.dto.CSVStatesCensus;
+import com.bridgelabz.dto.CSVStatesPojoClass;
+import com.bridgelabz.dto.CSVUSCensus;
+
+public class CensusDAO {
+    public float HousingDensity;
+    public String StateID;
+    public String State;
+    public long Population;
+    public long AreaInSqKm;
+    public int DensityPerSqkm;
+    public String StateCode;
+    public int TIN;
+    public int SrNo;
+
+    public CensusDAO(CSVStatesCensus csvStatesCensus) {
+        this.State = CSVStatesCensus.state;
+        this.Population = csvStatesCensus.Population;
+        this.AreaInSqKm = csvStatesCensus.AreaInSqKm;
+        this.DensityPerSqkm = csvStatesCensus.DensityPerSqkm;
+    }
+
+    public CensusDAO(CSVStatesPojoClass csvStatesPojoClass) {
+        this.State = csvStatesPojoClass.StateName;
+        this.SrNo = csvStatesPojoClass.SrNo;
+        this.TIN = csvStatesPojoClass.TIN;
+        this.StateCode = csvStatesPojoClass.StateCode;
+    }
+
+    public CensusDAO(CSVUSCensus csvusCensus){
+        this.StateID = csvusCensus.StateID;
+        this.State = csvusCensus.State;
+        this.Population = csvusCensus.Population;
+        this.AreaInSqKm= csvusCensus.Area;
+        this.DensityPerSqkm = csvusCensus.PopulationDensity;
+        this.HousingDensity = csvusCensus.HousingDensity;
+    }
+}
