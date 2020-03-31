41c82953755582b1dce428f6129e4b2451dce2c9
Revati Tekale <revitekale1910@gmail.com>
2020-03-31 20:56:30 +0530

    ADDED:Code to return Indian census csv as DTO to ensure encapsulation

--- src/main/java/com/bridgelabz/dao/CensusDAO.java
+++ src/main/java/com/bridgelabz/dao/CensusDAO.java
@@ -3,6 +3,9 @@ package com.bridgelabz.dao;
 import com.bridgelabz.dto.CSVStatesCensus;
 import com.bridgelabz.dto.CSVStatesPojoClass;
 import com.bridgelabz.dto.CSVUSCensus;
+import com.bridgelabz.service.CensusAnalyser;
+
+import java.util.Comparator;
 
 
 public class CensusDAO {
@@ -11,7 +14,7 @@ public class CensusDAO {
     public String State;
     public long Population;
     public long AreaInSqKm;
-    public int DensityPerSqkm;
+    public long DensityPerSqkm;
     public String StateCode;
     public int TIN;
     public int SrNo;
@@ -30,12 +33,56 @@ public class CensusDAO {
         this.StateCode = csvStatesPojoClass.StateCode;
     }
 
-    public CensusDAO(CSVUSCensus csvusCensus){
+    public CensusDAO(CSVUSCensus csvusCensus) {
         this.StateID = csvusCensus.StateID;
         this.State = csvusCensus.State;
         this.Population = csvusCensus.Population;
-        this.AreaInSqKm= csvusCensus.Area;
+        this.AreaInSqKm = csvusCensus.Area;
         this.DensityPerSqkm = csvusCensus.PopulationDensity;
         this.HousingDensity = csvusCensus.HousingDensity;
+    }
+
+    public static Comparator<CensusDAO> getSortComparator(CensusAnalyser.SortingMode mode) {
+        if (mode.equals(CensusAnalyser.SortingMode.STATE))
+            return Comparator.comparing(census -> census.State);
+        if (mode.equals(CensusAnalyser.SortingMode.POPULATION))
+            return Comparator.comparing(CensusDAO::getPopulation).reversed();
+        if (mode.equals(CensusAnalyser.SortingMode.AREA))
+            return Comparator.comparing(CensusDAO::getAreaInSqKm).reversed();
+        if (mode.equals(CensusAnalyser.SortingMode.DENSITY))
+            return Comparator.comparing(CensusDAO::getDensityPerSqkm).reversed();
+        return null;
+    }
+
+    public long getPopulation() {
+        return Population;
+    }
+
+    public void setPopulation(long population) {
+        Population = population;
+    }
+
+    public long getAreaInSqKm() {
+        return AreaInSqKm;
+    }
+
+    public void setAreaInSqKm(long areaInSqKm) {
+        AreaInSqKm = areaInSqKm;
+    }
+
+    public long getDensityPerSqkm() {
+        return DensityPerSqkm;
+    }
+
+    public void setDensityPerSqkm(int densityPerSqkm) {
+        DensityPerSqkm = densityPerSqkm;
+    }
+
+    public Object getCensusDTO(CensusAnalyser.Country country) {
+        if (country.equals(CensusAnalyser.Country.INDIA))
+            return new CSVStatesCensus(State, Population, AreaInSqKm, DensityPerSqkm);
+        if (country.equals(CensusAnalyser.Country.US))
+            return new CSVUSCensus(StateCode, State, Population, AreaInSqKm, DensityPerSqkm);
+        return null;
     }
 }
